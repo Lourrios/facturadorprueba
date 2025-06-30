8924b793492944ddde9726bf013766f9aab04f56
Ignacio Mersch <ignaciomersch602811@gmail.com>
2025-06-30 17:26:45 -0300

    #Add: tablas(clientes, facturas, pagos)

---
+++ database/migrations/2025_06_30_202445_create_pagos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pagos', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('monto', total:10, places:2);
+            $table->datetime('fecha_pago');
+            $table->string('metodo_pago');
+            $table->text('observaciones')->nullable();
+            $table->unsignedInteger('factura_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pagos');
+    }
+};

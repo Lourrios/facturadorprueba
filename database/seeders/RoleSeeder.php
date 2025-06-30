865b92017861c0190e51f86b9eb9049ef7d0e30d
Ignacio Mersch <ignaciomersch602811@gmail.com>
2025-06-30 18:19:25 -0300

    #Add: relacion rol-permiso en seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RoleSeeder extends Seeder
 {
@@ -13,7 +14,18 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::create(['name'=>'admin']);
-        Role::create(['name' => 'usuario']);
+        $admin = Role::create(['name'=>'admin']);
+        $usuario = Role::create(['name' => 'usuario']);
+
+        Permission::create(['name' => 'ver-rol'])->assignRole($admin);
+        Permission::create(['name' => 'crear-rol'])->assignRole($admin);
+        Permission::create(['name' => 'editar-rol'])->assignRole($admin);
+        Permission::create(['name' => 'borrar-rol'])->assignRole($admin);
+
+        Permission::create(['name' => 'ver-usuarios'])->assignRole($admin);
+        Permission::create(['name' => 'crear-usuarios'])->assignRole($admin);
+        Permission::create(['name' => 'editar-usuarios'])->assignRole($admin);
+        Permission::create(['name' => 'borrar-usuarios'])->assignRole($admin);
+        
     }
 }
